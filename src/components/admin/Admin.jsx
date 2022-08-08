158568d2c446e95a4e2536c794b4c3f79de7afea
Leonel Tammaro <tammaroleonel@gmail.com>
2022-08-08 20:47:35 -0300

    add auth0, add admin authentication

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Text } from "@chakra-ui/layout";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
+import userContext from "../../contexts/userContext";
 
 function Admin() {
+  const [loading, setLoading] = React.useState(true);
+  const { user, setUser } = React.useContext(userContext);
+  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && !user.user.isAdmin) {
+      const newUser = { ...user };
+      newUser.user.isAdmin = isAuthenticated;
+      setUser(newUser);
+      setLoading(false);
+    }
+  }, [user, isAuthenticated, isLoading]);
+
+  if (isLoading || loading) return <Text>Is loading....</Text>;
+  if (!isAuthenticated) return loginWithRedirect();
+  if (!user.user.isAdmin) navigate("/user");
   return <div>Admin</div>;
 }
 
