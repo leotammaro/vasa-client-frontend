ebe1a49aac3466ed0f85997cd17a9296182db5f3
Leo <52765886+leotammaro@users.noreply.github.com>
2022-08-08 21:10:58 -0300

    authentication (#3)
    * add auth0, add admin authentication
    
    * remove unnecesary console.log and remove component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Admin from "./components/admin/Admin";
-import LogInUser from "./components/user/LogInUser";
 import User from "./components/user/User";
+import userContext from "./contexts/userContext.jsx";
 
 function App() {
   const [user, setUser] = React.useState({
     isLogged: false,
     isLoading: false,
     user: {
-      isAdmin: false,
+      isAdmin: null,
     },
   });
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/user" element={<User />} />
-          <Route
-            path="/"
-            element={
-              user.isLogged && !user.user.isAdmin ? <User /> : <LogInUser />
-            }
-          />
-        </Routes>
+        <userContext.Provider value={{ user, setUser }}>
+          <Routes>
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/user" element={<User />} />
+            <Route path="/" element={<User />} />
+          </Routes>
+        </userContext.Provider>
       </BrowserRouter>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-a5t7pzgz.us.auth0.com"
+      clientId="utcl8PdXikiTa8OaYE36uTfTvOsSwyKZ"
+      redirectUri={window.location.origin + "/admin"}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
