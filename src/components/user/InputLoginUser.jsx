f9ea4369278f8b21f2a68d0555ed8e4642af67f8
Leonel Tammaro <tammaroleonel@gmail.com>
2022-08-09 23:13:58 -0300

    add props to InputLoginUser component. fixed props values

--- src/components/user/InputLoginUser.jsx
+++ src/components/user/InputLoginUser.jsx
@@ -1,11 +1,9 @@
 import { Image } from "@chakra-ui/image";
 import { Input } from "@chakra-ui/input";
 import { Flex } from "@chakra-ui/layout";
-import lock from "../../assets/lock.svg";
-import user from "../../assets/user.svg";
 import React from "react";
 
-function InputLoginUser({ inputType }) {
+function InputLoginUser({ inputType, iconSrc, placeholder }) {
   return (
     <Flex
       border="1px solid #B6C2E2"
@@ -15,18 +13,13 @@ function InputLoginUser({ inputType }) {
       w="100%"
       maxW={490}
     >
-      <Image
-        src={inputType === "password" ? lock : user}
-        h={15}
-        paddingRight={5}
-        opacity={0.8}
-      />
+      <Image src={iconSrc} h={15} paddingRight={5} opacity={0.8} />
       <Input
-        placeholder={inputType === "password" ? "Contraseña" : "Usuario"}
+        placeholder={placeholder}
         border="none"
         fontSize={13}
         _focusVisible={{ outline: "none" }}
-        type={inputType === "password" ? "password" : "text"}
+        type={inputType}
         flex={1}
       />
     </Flex>
